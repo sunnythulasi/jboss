6d8b365d16c868e0d91aa7ac6c807ea70438038d
root <root@ip-172-31-5-212.us-west-2.compute.internal>
2017-03-29 13:01:09 +0000

    addedfiles

--- scripts/jbossdeploy.sh
+++ scripts/jbossdeploy.sh
@@ -1,1 +1,1 @@
-/opt/jboss/bin/jboss-cli.sh --connect --controller=localhost:9999 --user=jbossdeploy --password=jbossdeploy@123 --commands="deploy /home/ubuntu/releases --runtime-name=helloworld.war"
+/opt/jboss/bin/jboss-cli.sh --connect --controller=localhost:9999 --user=jbossdeploy --password=jbossdeploy@123 --commands="deploy /home/ubuntu/releases/hello-world.war --runtime-name=helloworld.war"

2bc3af7f221fc2c938d805328d3c1bb4dc751925
root <root@ip-172-31-5-212.us-west-2.compute.internal>
2017-03-29 12:32:15 +0000

    jbossdeploy

---
+++ scripts/jbossdeploy.sh
@@ -0,0 +1,1 @@
+/opt/jboss/bin/jboss-cli.sh --connect --controller=localhost:9999 --user=jbossdeploy --password=jbossdeploy@123 --commands="deploy /root/awscodedeploy/hello-world.war --runtime-name=helloworld"

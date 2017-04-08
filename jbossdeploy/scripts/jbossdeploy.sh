b3b6a158a8f47d1e3c968cb8489afd77a8c4f67e
root <root@ip-172-31-23-220.us-west-2.compute.internal>
2017-04-08 07:17:57 +0000

    first commit

---
+++ jbossdeploy/scripts/jbossdeploy.sh
@@ -0,0 +1,1 @@
+/opt/jboss/bin/jboss-cli.sh --connect --controller=localhost:9999 --user=jbossdeploy --password=jbossdeploy@123 --commands="deploy /home/ubuntu/releases/hello-world.war --runtime-name=helloworld.war"
